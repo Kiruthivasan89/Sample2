f3f9afa7ec812c239a463b8643e5505515932570
Kiruthivasan89 <80023192+Kiruthivasan89@users.noreply.github.com>
2021-05-08 10:06:16 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+
+int f=5, fact=1;
+
+
+	for (int i=1; i<=f; i++) {
+		
+		fact=fact*i;
+		
+		
+	}
+	System.out.println("The Factorial number is: "+fact);
+	
+	}
+
+
+}
